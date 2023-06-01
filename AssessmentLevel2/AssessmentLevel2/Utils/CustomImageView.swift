5b2a57ce7220fc99ef85a34434e8eb587be466b5
vparjunmohan <gomathisankarcg@gmail.com>
2023-06-01 23:56:33 +0530

    fixed image loading issue

--- AssessmentLevel2/AssessmentLevel2/Utils/CustomImageView.swift
+++ AssessmentLevel2/AssessmentLevel2/Utils/CustomImageView.swift
@@ -11,33 +11,22 @@ import UIKit
 class CustomImageView: UIImageView {
     
     var task: URLSessionDataTask!
-    var imageCache = NSCache<AnyObject, AnyObject>()
     
     func loadImage(urlString: String) {
         self.image = nil
-        
         if let task = task {
             task.cancel()
         }
         let url = URL(string: urlString)
         guard let url else { return }
-        
-        if let imageFromCache = imageCache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
-            self.image = imageFromCache
-            return
-        }
-        
         task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            guard let self = self, let data = data, let newImage = UIImage(data: data) else {
+            guard let self = self, let data = data, let newImage = UIImage(data: data)?.resizeImage(toMaxDimension: 100) else {
                 print("Cannot load image from url: \(url)")
                 return
             }
-            
-            imageCache.setObject(newImage, forKey: url.absoluteString as AnyObject)
-            
             DispatchQueue.main.async { [weak self] in
                 guard let self else { return }
-                self.image = newImage.resizeImage(toMaxDimension: 500)
+                self.image = newImage
             }
         }
         task.resume()
