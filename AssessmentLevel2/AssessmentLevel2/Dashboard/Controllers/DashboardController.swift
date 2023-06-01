ef06c72a2a3c35b05100b01a2c38d59af21246cd
vparjunmohan <gomathisankarcg@gmail.com>
2023-06-01 16:28:26 +0530

    initial commit

---
+++ AssessmentLevel2/AssessmentLevel2/Dashboard/Controllers/DashboardController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentLevel2
+//
+//  Created by Sankar on 01/06/23.
+//
+
+import UIKit
+
+class DashboardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AssessmentLevel2/AssessmentLevel2/Dashboard/Model/DashboardModel.swift
@@ -0,0 +1,8 @@
+//
+//  DashboardModel.swift
+//  AssessmentLevel2
+//
+//  Created by Sankar on 01/06/23.
+//
+
+import Foundation
